c16c43ed1b94e55023ee2552ff9bca962965f0f4
shalommarimi <shalommarimi@gmail.com>
2017-06-29 11:37:42 +0200

    Fixed Connection string

---
+++ DAL/Operations/CRUD/Test/DeleteDepartment.cs
@@ -0,0 +1,46 @@
+﻿//using Dataaccess;
+//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+
+//namespace CodeFirstEntityFramework.Operations
+//{
+//    class DeleteDepartment
+//    {
+//        public void Delete_Department(int PK_DepartmentId)
+//        {
+//            using (var usersDBContext = new UsersDBContext())
+//            {
+//                try
+//                {
+//                    var _ObjUpdateDepartment = new Department();
+//                    _ObjUpdateDepartment = usersDBContext.departments.SingleOrDefault(x => x.PK_DepartmentId == PK_DepartmentId);
+//                    usersDBContext.departments.Remove(_ObjUpdateDepartment);
+//                    usersDBContext.SaveChanges();
+//                    Console.WriteLine("Department Deleted");
+//                    Console.ReadKey();
+//                }
+//                catch (Exception)
+//                {
+
+//                    Console.WriteLine("Could not delete Department, Department with ID " + PK_DepartmentId + " may not exist or there is a Referential Integrity constraint");
+//                    Console.ReadKey();
+//                }
+
+//            }
+//        }
+
+//        public void EnterDepartId()
+//        {
+//            var _ObjUpdateDepartment = new UpdateDepartment();
+
+//            Console.WriteLine("Please enter Department ID to Delete");
+//            int PK_Departmentid = Convert.ToInt32(Console.ReadLine());
+            
+//            Delete_Department(PK_Departmentid);
+//        }
+
+//    }
+//}
---
+++ DAL/Operations/CRUD/Test/InsertDepartment.cs
@@ -0,0 +1,57 @@
+﻿//using System;
+//using Dataaccess;
+
+
+
+
+//namespace CodeFirstEntityFramework.Operations
+//{
+//    class InsertDepartment
+//    {
+//        public void EntertDepartmentDetails()
+//        {
+
+            
+//            Console.WriteLine("Please enter Department Name");
+//            string _departmentName = Console.ReadLine();
+
+//            Console.WriteLine("Please enter Department Description");
+//            string _departmentDescription = Console.ReadLine();
+
+//            Console.WriteLine("\r\n");
+
+
+//            var _ObjInsertDepartment = new InsertDepartment();
+//           _ObjInsertDepartment.Insert_Department(_departmentName, _departmentDescription);
+
+//        }
+
+//        public void Insert_Department(string _departmentName, string _departmentDescription)
+//        {
+//            using (var usersDBContext = new UsersDBContext())
+//            {
+//                try
+//                {
+//                    Department _Objdepartment = new Department();
+//                    _Objdepartment.DepartmentName = _departmentName;
+//                    _Objdepartment.DepartmentDescription = _departmentDescription;
+//                    usersDBContext.departments.Add(_Objdepartment);
+//                    usersDBContext.SaveChanges();
+
+//                    Console.WriteLine(" A new Department has been inserted");
+//                    Console.ReadKey();
+//                }
+//                catch (Exception)
+//                {
+
+//                    Console.WriteLine("Could not insert new Department");
+//                }
+            
+//            }
+
+          
+
+//        }
+        
+//    }
+//}
