c91baa032be45fe2313f5b756de4ac987787957a
shalommarimi <shalommarimi@gmail.com>
2017-06-29 16:02:08 +0200

    Fixes

--- DAL/Operations/Program.cs
+++ DAL/Operations/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using DAL.DBContext;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +11,9 @@ namespace Operations
     {
         static void Main(string[] args)
         {
+
+            var _ObjInsertPhysicalAddress = new InsertPhysicalAddress();
+            _ObjInsertPhysicalAddress.EnterPhysicalAddressDetails();
         }
     }
 }
