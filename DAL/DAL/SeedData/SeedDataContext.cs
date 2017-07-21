9a326a8f2cdd5b1bfc351ca56f3a12a8906506b2
Simphiwe Nkutha <SNkutha@jhb.dvt.co.za>
2017-07-21 10:44:22 +0200

    Seed Method completed

--- DAL/DAL/SeedData/SeedDataContext.cs
+++ DAL/DAL/SeedData/SeedDataContext.cs
@@ -28,9 +28,56 @@ namespace DAL.DBContext
                 context.Gender.Add(gen);
 
 
+            //Inserting Provinces
+            IList<Province> provinces = new List<Province>();
+            provinces.Add(new Province() { ProvinceName = "Gauteng" });
+            provinces.Add(new Province() { ProvinceName = "Limpopo" });
+            provinces.Add(new Province() { ProvinceName = "Free State" });
+            provinces.Add(new Province() { ProvinceName = "Northen Cape" });
+            provinces.Add(new Province() { ProvinceName = "North West" });
+            provinces.Add(new Province() { ProvinceName = "Kwa-Zulu Natal" });
+            provinces.Add(new Province() { ProvinceName = "Mpumalanga" });
+            provinces.Add(new Province() { ProvinceName = "Western Cape" });
+            provinces.Add(new Province() { ProvinceName = "Eastern Cape" });
 
-            //UserType, Normal--Sdmin
-            //Province
+            foreach (Province pro in provinces)
+                context.Province.Add(pro);
+
+
+
+
+            IList<City> cities = new List<City>();
+            cities.Add(new City() { CityName = "Johannesburg" });
+            cities.Add(new City() { CityName = "Polokwane" });
+            cities.Add(new City() { CityName = "Bloemfontein" });
+            cities.Add(new City() { CityName = "Upington" });
+            cities.Add(new City() { CityName = "Mahikeng" });
+            cities.Add(new City() { CityName = "Durban" });
+            cities.Add(new City() { CityName = "Witbank" });
+            cities.Add(new City() { CityName = "Cape Town" });
+            cities.Add(new City() { CityName = "East London" });
+
+
+            foreach (City ci in cities)
+                context.City.Add(ci);
+
+
+
+            IList<Suburb> suburbs = new List<Suburb>();
+            suburbs.Add(new Suburb() { SuburbName = "Ridgeway" });
+            suburbs.Add(new Suburb() { SuburbName = "Jane Furse" });
+            suburbs.Add(new Suburb() { SuburbName = "Willows" });
+            suburbs.Add(new Suburb() { SuburbName = "Flora Park" });
+            suburbs.Add(new Suburb() { SuburbName = "Danville" });
+            suburbs.Add(new Suburb() { SuburbName = "Ballito" });
+            suburbs.Add(new Suburb() { SuburbName = "Willows" });
+            suburbs.Add(new Suburb() { SuburbName = "Atlantic Seaboard" });
+            suburbs.Add(new Suburb() { SuburbName = "Beacon Bay" });
+
+
+            foreach (Suburb sub in suburbs)
+                context.Suburb.Add(sub);
+
 
 
 
